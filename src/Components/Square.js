5ed66892c0161c075f4849fc75e3435b18c1025f
knrtaven <52444839+knrtaven@users.noreply.github.com>
2022-10-16 20:28:38 +0800

    base func for promotions and gameover. to be merged

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,17 +3,22 @@ import Piece from "./Piece"
 import { useDrop } from 'react-dnd'
 import { useContext } from 'react';
 import ChessContext from "../Context";
-
 export default function Square(props) {
   const {piece, bgColor, position} = props
-  const { Move } = useContext(ChessContext)
+  const { Move, toPromote,} = useContext(ChessContext)
 
   const [ , drop] = useDrop({
     accept: "piece",
     drop: (item) => {
-      const [fromPosition] = item.id.split("_")
+      const [fromPosition, type, color] = item.id.split("_")
+      
+      if ((color === 'w' && position.includes("8") )||(color === 'b' && position.includes("1"))) {
+        toPromote(fromPosition, position, 'q')
+        console.log(true)
+      }
       Move(fromPosition, position)
       console.log(fromPosition, position)
+
     }
   })
 
